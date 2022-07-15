bab2b0370a89c9f80b27af30f1aa54dbd560f2e0
Zach Obront <zobront@gmail.com>
2022-07-15 13:38:56 -0500

    validate path

---
+++ src/http/response.rs
@@ -0,0 +1,27 @@
+use super::StatusCode;
+use std::fmt::{Display, Formatter, Result as FmtResult};
+use std::net::TcpStream;
+use std::io::{Write, Result as IoResult};
+
+#[derive(Debug)]
+pub struct Response {
+    status_code: StatusCode,
+    body: Option<String>,
+}
+
+impl Response {
+    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+        Self {
+            status_code,
+            body,
+        }
+    }
+
+    pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
+        let body = match &self.body {
+            Some(body) => body,
+            None => "",
+        };
+        write!(stream, "HTTP/1.1 {} {}\r\n\r\n{}", self.status_code, self.status_code.reason_phrase(), body)
+    }
+}
---
+++ src/server.rs
@@ -0,0 +1,55 @@
+use std::net::TcpListener;
+use std::io::{Read, Write};
+use crate::http::{Request, Response, StatusCode, ParseError};
+use std::convert::TryFrom;
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+
+}
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    pub fn run(self, mut handler: impl Handler) {
+        println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+        
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(size) => {
+                                println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                                let response = match Request::try_from(&buffer[..]) {
+                                    Ok(request) => handler.handle_request(&request),
+                                    Err(e) => handler.handle_bad_request(&e), 
+                                };
+
+                                if let Err(e) = response.send(&mut stream) {
+                                    println!("Failed to send response: {}", e);
+                                }
+
+                            },
+                        Err(e) => println!("Error: {}", e),
+                    }
+                },
+                Err(e) => println!("Error: {}", e)
+            } 
+        }
+    }
+}
